b7d3a9474c5a42095b37d51c397aed67250489d5
Chiri Vulpes <chiri@chiri.works>
2025-09-14 20:43:48 +1200

    Tweak stuff, log everything in the docs/generated dirs

--- task/watch.ts
+++ task/watch.ts
@@ -1,13 +1,13 @@
 import { Task } from 'task'
+import build from './build'
 import inspect from './inspect'
-import _static from './static'
 import Hash from './utility/Hash'
 
 export default Task('watch', async task => {
 	task.watch(['static/**/*', './task/generate_enums.ts', './task/deepsight_manifest.ts', './task/manifest/**/*'], async (event, path) => true
 		&& !path?.endsWith('Enums.d.ts') && !path?.endsWith('DeepsightPlugCategorisation.d.ts')
 		&& (await Hash.fileChanged(path))
-		&& task.debounce(_static, path))
+		&& task.debounce(build, path))
 
 	await Promise.all([
 		task.run(inspect),
