9825e3d30e163f8f944061ab5a702e437aa92f4f
Chiri Vulpes <chiri@chiri.works>
2025-10-09 20:26:57 +1300

    Update task & lint packages

---
+++ task/install.ts
@@ -0,0 +1,11 @@
+import { Task } from 'task'
+
+export default Task('install', async task => {
+	await task.install({
+		path: '.',
+		dependencies: {
+			lint: { repo: 'fluff4me/lint' },
+			task: { repo: 'chirivulpes/task', branch: 'package' },
+		},
+	})
+})
