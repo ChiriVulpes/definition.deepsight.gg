718868690992cd22389d3cb70ac8fb833ff48750
Chiri Vulpes <chiri@chiri.works>
2026-07-04 11:43:37 +1200

    Exclude profile.json updates from watch task

--- task/watch.ts
+++ task/watch.ts
@@ -4,10 +4,14 @@ import inspect from './inspect'
 import Hash from './utility/Hash'
 
 export default Task('watch', async task => {
-	task.watch(['static/**/*', './task/generate_enums.ts', './task/deepsight_manifest.ts', './task/manifest/**/*'], async (event, path) => true
-		&& !path?.endsWith('Enums.d.ts') && !path?.endsWith('DeepsightPlugCategorisation.d.ts')
-		&& (await Hash.fileChanged(path))
-		&& task.debounce(build, path))
+	task.watch(['static/**/*', './task/generate_enums.ts', './task/deepsight_manifest.ts', './task/manifest/**/*'], async (event, path) => {
+		const file = path?.replace(/\\/g, '/')
+		return true
+			&& file !== 'static/testiny/profile.json'
+			&& !file?.endsWith('Enums.d.ts') && !file?.endsWith('DeepsightPlugCategorisation.d.ts')
+			&& (await Hash.fileChanged(path))
+			&& task.debounce(build, path)
+	})
 
 	await Promise.all([
 		task.run(inspect),
